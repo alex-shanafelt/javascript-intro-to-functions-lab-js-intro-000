cd7fce928ab33bd0a8de6a7bf32619266dc81710
Alex Shanafelt <alexpshanafelt@gmail.com>
2021-05-19 19:51:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,8 +27,8 @@ function sayHiToGrandma(string) {
     return "I can't hear you!"
   }
 
-  else if (string === "I love you, Grandma") {
-    return "I love you, too"
+  else if (string === "I love you, Grandma.") {
+    return "I love you, too."
   }
 
   else {}
